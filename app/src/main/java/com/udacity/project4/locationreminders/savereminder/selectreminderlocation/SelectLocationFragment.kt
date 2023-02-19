dd2e0632dc18ba8354ea873472878772f1cbdde9
Mohamed Alaa <mohamedalaa4j@gmail.com>
2023-02-19 18:12:13 +0200

    Get last location and show on the map done

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,15 +1,28 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.*
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
@@ -17,16 +30,45 @@ import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
+
 class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+
     private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var userLatitude: Double? = null
+    private var userLongitude: Double? = null
+
+    //region Request permissions API
+    private val requestLocationPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                Toast.makeText(requireContext(), "Location Permissions Granted", Toast.LENGTH_SHORT)
+                    .show()
+                getLocation()
+
+            } else if (it[Manifest.permission.ACCESS_COARSE_LOCATION] == true) {
+                Toast.makeText(
+                    requireContext(),
+                    "Precise location is highly recommended for full app features",
+                    Toast.LENGTH_SHORT
+                ).show()
+                getLocation()
+
+            } else {
+                Toast.makeText(requireContext(), "Permissions denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    //endregion
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_select_location, container, false)
 
@@ -36,11 +78,15 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-//        TODO: add the map setup implementation
+        showInstructionsDialog()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        // add the map setup implementation
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-//        TODO: zoom to the user location after taking his permission
+        // zoom to the user location after taking his permission
+        checkIfPermissionsGrantedOrNot()
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
 
@@ -50,13 +96,6 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
         return binding.root
     }
-
-    private fun onLocationSelected() {
-        //        TODO: When the user confirms on the selected location,
-        //         send back the selected location details to the view model
-        //         and navigate back to the previous fragment to save the reminder and add the geofence
-    }
-
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.map_options, menu)
@@ -79,13 +118,98 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         else -> super.onOptionsItemSelected(item)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap?) {
         map = googleMap!!
-
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.isMyLocationEnabled = true
+        getLocation()
     }
 
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
 
+            if (location != null) {
+                userLatitude = location.latitude
+                userLongitude = location.longitude
+
+                //show the location on the map
+                val locationObject = LatLng(userLatitude!!, userLongitude!!)
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(locationObject, 13f))
+
+
+            }
+        }
+
+    }
+
+    private fun checkIfPermissionsGrantedOrNot() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Request permissions if not
+            requestLocationPermissions.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+
+        } else {
+            // If permissions is granted check the GPS & request location
+            if (isGPSEnabled()) {
+                getLocation()
+            } else {
+                Toast.makeText(
+                    context,
+                    "Location is disabled please enable it from the settings",
+                    Toast.LENGTH_SHORT
+                ).show()
+                requireActivity().startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+        }
+    }
+
+    private fun isGPSEnabled(): Boolean {
+        val locationManager =
+            activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun onLocationSelected() {
+        //        TODO: When the user confirms on the selected location,
+        //         send back the selected location details to the view model
+        //         and navigate back to the previous fragment to save the reminder and add the geofence
+    }
+
+    private fun showInstructionsDialog() {
+
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        dialogBuilder.setMessage("Choose a location on the map")
+            .setCancelable(true)
+
+            .setPositiveButton("OK") { _, _ ->
+            }
+
+        // create dialog box
+        val alert = dialogBuilder.create()
+
+        // set title for alert dialog box
+        alert.setTitle("Tips")
+
+        alert.show()
+    }
 }
+
+
