fbea469d4d2be0726ea67b625fde62f60213f06a
Mohamed Alaa <mohamedalaa4j@gmail.com>
2023-02-24 15:42:59 +0200

    Failed to resolve: com.google.firebase:firebase-auth bug when androidTests run fixed

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -3,21 +3,18 @@ package com.udacity.project4.locationreminders.data.local
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.runner.RunWith;
-
-import kotlinx.coroutines.ExperimentalCoroutinesApi;
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -25,6 +22,40 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    // Add testing implementation to the RemindersDao.kt
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun initDb() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun saveReminderAndGetReminderById() = runBlockingTest {
+        // GIVEN - Insert a task.
+        val reminder1 = ReminderDTO("Title1", "Description1", "Location1", 29.976480, 31.131302)
+        database.reminderDao().saveReminder(reminder1)
+
+        // WHEN - Get the task by id from the database.
+        val loaded = database.reminderDao().getReminderById(reminder1.id)
+
+        // THEN - The loaded data contains the expected values.
+        assertThat(loaded?.id, `is`(reminder1.id))
+        assertThat(loaded?.title, `is`(reminder1.title))
+        assertThat(loaded?.description, `is`(reminder1.description))
+        assertThat(loaded?.location, `is`(reminder1.location))
+        assertThat(loaded?.latitude, `is`(reminder1.latitude))
+        assertThat(loaded?.longitude, `is`(reminder1.longitude))
+    }
 
 }
