07723444f673732c30c04a0d38f16578ff05d842
Mohamed Alaa <mohamedalaa4j@gmail.com>
2023-02-26 05:14:53 +0200

    RemindersActivityTested

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,28 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.EspressoIdlingResource
+import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -27,6 +39,7 @@ class RemindersActivityTest :
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -65,7 +78,54 @@ class RemindersActivityTest :
         }
     }
 
+    @After
+    fun stopKoinAfterTest() {
+        stopKoin()
+    }
 
-//    TODO: add End to End testing to the app
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
 
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    //    TODO: add End to End testing to the app
+    @Test
+    fun launchTheApp_fragmentContainerIsDisplayed() {
+
+        // GIVEN - RemindersActivity.
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+
+        // monitor the fragment for idling resources
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // WHEN - RemindersActivity opens
+
+        // THEN - nav_host_fragment is displayed
+        onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun remindersActivityOpens_logoutIsDisplayed() {
+        // GIVEN - RemindersActivity (After user login).
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+
+        // monitor the fragment for idling resources
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // WHEN - RemindersActivity opens
+
+        // THEN - nav_host_fragment is displayed
+        onView(withId(R.id.logout)).check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
 }
